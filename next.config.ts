f4d042533208e49813febbaddc1c3d17c7fc01e3
Mark <mark@markwarddesign.com>
2025-11-06 17:31:19 -0700

    update settings

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'export',
-  basePath: '/dealertransparency',
   images: {
     unoptimized: true,
   },
